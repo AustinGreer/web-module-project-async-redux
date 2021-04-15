dfa0d67fd91adca5f00bd77cb870368700ed5ac8
Austin Greer <agreer.ag12@gmail.com>
2021-04-15 17:40:43 -0500

    spun up react app, did some clean up, added pokemon gen 1 url

---
+++ pokedex/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Gotta Catch Them All!</h1>
+    </div>
+  );
+}
+
+export default App;
+
+// GEN 1 URL - https://pokeapi.co/api/v2/pokemon?limit=151
