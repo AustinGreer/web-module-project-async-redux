b4925d02b9578afbadfe770cd136f0b1a1ff58c4
Austin Greer <agreer.ag12@gmail.com>
2021-04-15 22:03:01 -0500

    built redux store

---
+++ pokedex/src/store/reducers/index.js
@@ -0,0 +1,13 @@
+const initialState = {
+    pokemon: [],
+    fetchStart: false,
+    fetchSuccess: false,
+    fetchFail: false
+}
+
+export const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state
+    }
+}
